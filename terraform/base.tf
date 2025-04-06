de0f5a007b507bbb8eb6bab7333a35437b9ea192
Steven Buglione <stevenbuglione1@gmail.com>
2025-04-05 22:30:30 -0400

    Working Creation and Deletion Of LXD Cluster and Container With Ansible & Terraform

--- terraform/base.tf
+++ terraform/base.tf
@@ -1,12 +1,38 @@
-resource "lxd_instance" "instance1" {
-  name  = "instance1"
-  image = "ubuntu-daily:22.04"
+resource "lxd_instance" "ssh_box" {
+  count  = 8
+  name   = "dev-ssh-box-${count.index}"
+  image  = "ubuntu-daily:22.04"
 
   config = {
-    "boot.autostart" = true
+    "user.user-data" = <<EOF
+#cloud-config
+users:
+  - name: ubuntu
+    shell: /bin/bash
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    ssh-authorized-keys:
+      - ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIADXEW0ESKUfvgzAYIuHH/Rehcvhm8j4op7VlpLClfvC
+package_update: true
+package_upgrade: true
+packages:
+  - openssh-server
+runcmd:
+  - systemctl enable ssh
+  - systemctl restart ssh
+  - ufw allow 22/tcp
+  - ufw reload
+EOF
   }
 
-  limits = {
-    cpu = 2
+  device {
+    name       = "eth0"
+    type       = "nic"
+    properties = {
+      nictype = "macvlan"
+      parent  = "eth0"  // Replace with your actual physical interface if different.
+    }
   }
 }
+
+
+
--- terraform/config/dev.tfvars
+++ terraform/config/dev.tfvars
@@ -1,7 +1,4 @@
 lxd_provider_config = {
   name = "klustered"
   address = "https://10.10.10.5:8443"
-  client_cert = "./.tmp/lxd.crt"
-  client_key = "./.tmp/lxd.key"
-  accept_remote_certificate = true
 }
