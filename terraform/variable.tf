f631c1ea2e1bebf2a7c50883c3fef2e319f90647
Steven Buglione <stevenbuglione1@gmail.com>
2025-04-06 18:46:36 -0400

    Working Refactored LXC K3s Cluster With Terraform

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -5,3 +5,46 @@ variable "lxd_provider_config"{
     address = string
   })
 }
+
+variable "server_nodes" {
+  type = list(object({
+    name = string
+    ip = string
+  }))
+  description = "List of server nodes with name and IP"
+}
+
+variable "agent_nodes" {
+  type = list(object({
+    name = string
+    ip = string
+  }))
+  description = "List of agent nodes with name and IP"
+  default = []
+}
+
+variable "lxc_cloud_init_configuration"{
+  description = "Configuration used to configure lxc cloud init image"
+  type = object({
+    user = object({
+      name = string
+      shell = string
+      sudo = string
+      ssh_authorized_key = list(string)
+    })
+    package_update = bool
+    package_upgrade = bool
+    packages = list(string)
+    runcmd = list(string)
+  })
+}
+
+variable "lxc_network_gateway4"{
+  description = "Configuration for lxc instance gateway4 ip address"
+  type = string
+}
+
+variable "lxc_network_nameserver_addresses"{
+  description = "Configuration for lxc nameserver ip addresses"
+  type = list(string)
+}
